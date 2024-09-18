7d9a0688a2cd89ab9e53f2fb234cae1ac27ab256
jazst21 <p.jayanegara@gmail.com>
2024-09-18 23:08:09 +0700

    restructure pypi standard

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
+    name='karpentermig',
     version='0.1',
-    packages=find_packages(),
-    description='A simple example package',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    description='A tool for Karpenter migration',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Your Name',
     license='MIT',
+    install_requires=[
+        'questionary',
+        'boto3',
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'karpentermig=karpentermig.cli:cli',
+        ],
+    },
 )
---
+++ src/karpentermig/cli.py
@@ -0,0 +1,18 @@
+import questionary
+from src.karpentermig.discover_cluster import export_eks_config  # Changed this line
+
+questions = [
+    {
+        'type': 'list',
+        'name': 'karpenter_migration',
+        'message': 'Select option:',
+        'choices': ['Discover eks cluster nodegroup config', 'Discover deployment config in namespace', 'Generate karpenter config', 'convert deployment to karpenter']
+    }
+]
+
+answers = questionary.prompt(questions)
+
+if answers['karpenter_migration'] == 'Discover eks cluster nodegroup config':
+    export_eks_config()
+else:
+    print(f"Selected option: {answers['karpenter_migration']}")
